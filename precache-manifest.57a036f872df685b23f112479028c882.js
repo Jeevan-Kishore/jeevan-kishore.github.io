22ac5ae960604e5ee68a696901c84a9ef71aa598
jeevan-kishore <kishor080@gmail.com>
2020-05-09 20:08:36 +0530

    feat(gp): Add github page build :sparkles:

---
+++ precache-manifest.57a036f872df685b23f112479028c882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad57b655c3fe212d4e3101eec68db82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e836c1f852b6c5f63a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ca57134cdf84c6dce3ea",
+    "url": "/static/js/2.870dd9fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.870dd9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e836c1f852b6c5f63a",
+    "url": "/static/js/main.0ac34075.chunk.js"
+  },
+  {
+    "revision": "0d1967961749aeefe9fc",
+    "url": "/static/js/runtime-main.f5b50921.js"
+  }
+]);
